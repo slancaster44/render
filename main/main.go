d6325ed958e205c45ca43b5576fd641fb9cb993c
Stanley <lancasterharp@gmail.com>
2023-07-11 15:22:30 -0500

    Progress

--- main/main.go
+++ main/main.go
@@ -1,30 +1,45 @@
 package main
 
 import (
-	"render"
-	"time"
+	"image/color"
+	"render/ray_tracer"
+	"render/tuple"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 )
 
+const WIDTH int = 900
+const HEIGHT int = 550
+
 func main() {
 	renderApp := app.New()
 	renderWindow := renderApp.NewWindow("Render Canvas")
-	renderContext := render.NewRayTracer(900, 550)
-	renderWindow.Resize(fyne.NewSize(900, 550))
+	renderContext := ray_tracer.NewRayTracer(WIDTH, HEIGHT)
+	renderContext.ReadFile("teapot.obj")
 
-	go func() {
-		img := renderContext.Render()
-		renderRaster := canvas.NewRasterFromImage(img)
-		renderWindow.SetContent(renderRaster)
+	l := ray_tracer.NewLight(tuple.NewPnt3(100, 100, 100), color.RGBA{255, 255, 255, 255})
+	renderContext.AddLight(l)
 
+	m := ray_tracer.Material{color.RGBA{128, 255, 255, 255}, 0.1, 0.9, 0.9, 200}
+	t := ray_tracer.Triangle{
+		tuple.NewPnt3(0, 0, 0),
+		tuple.NewPnt3(0, 1, 4),
+		tuple.NewPnt3(5, 0, 0),
+		&m,
+	}
+	renderContext.AddShape(&t)
+
+	renderWindow.Resize(fyne.NewSize(float32(WIDTH), float32(HEIGHT)))
+	renderRaster := canvas.NewRaster(renderContext.Render)
+	renderWindow.SetContent(renderRaster)
+
+	/*go func() {
 		for range time.Tick(time.Second / 60) {
-			img = renderContext.Render()
 			renderRaster.Refresh()
 		}
-	}()
+	}()*/
 
 	renderWindow.CenterOnScreen()
 	renderWindow.ShowAndRun()
--- utility/math.go
+++ utility/math.go
@@ -4,5 +4,7 @@ var sinTable [256]float64
 var cosTable [256]float64
 
 func genLookupTables() {
+	for i := 0; i < 256; i++ {
 
+	}
 }
